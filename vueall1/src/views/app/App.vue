1a234dfa2ed3a9f0744a940dd81d5b91c96d982a
idesignerSe <hebera75@gmail.com>
2022-03-14 17:17:14 +0100

    Exercise 1
    Router need to change address

--- vueall1/src/views/app/App.vue
+++ vueall1/src/views/app/App.vue
@@ -8,12 +8,14 @@
 
 <style lang="scss">
 #app {
+  background-color: aqua;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
+
 
 nav {
   padding: 30px;
